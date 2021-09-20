7a1e1b7d6e7671fd8e819bcce9085dc2795d31cf
Sonadi Kannangara <ksonadi@gmail.com>
2021-09-20 09:17:28 -0400

    Add project files.

---
+++ OOP3200-F2021-Lesson3/Vector2D.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef __VECTOR2D__
+#define __VECTOR2D__
+
+class Vector2D
+{
+public:
+	// Constructors
+
+	//Destructor
+
+	//Getters
+	
+	//Setters
+
+	//Public Methods (Class Functions)
+
+private:
+	//instance variables (Class Member Variables)
+
+	//Private methods (Private class functions)
+};
+
+#endif //define(__VECTOR_2D)
---
+++ OOP3200-F2021-Lesson3/main.cpp
@@ -0,0 +1,16 @@
+// OOP3200-F2021-Lesson3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*Author: Sonadi Kannangara
+ * Student ID: 100778336
+ * Date: 20 September 2021
+ * Description: Demo project for OOP3200 - F2021 - Week 3
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
