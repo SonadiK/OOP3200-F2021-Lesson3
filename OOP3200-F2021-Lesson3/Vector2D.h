a634e11bbb8070bfb7951177bd324b2cdb47f570
Sonadi Kannangara <ksonadi@gmail.com>
2021-09-20 09:42:49 -0400

    First draft of Vector2D class

--- OOP3200-F2021-Lesson3/Vector2D.h
+++ OOP3200-F2021-Lesson3/Vector2D.h
@@ -2,24 +2,38 @@
 
 #ifndef __VECTOR2D__
 #define __VECTOR2D__
+#include <string>
 
 class Vector2D
 {
 public:
 	// Constructors
+	//Default constructor
+	Vector2D();
 
 	//Destructor
+	~Vector2D();
 
 	//Getters
-	
+	float GetX() const;
+	float GetY() const;
+
 	//Setters
+	void SetX(float x);
+	void SetY(float y);
+	void Set(float x, float y);
 
 	//Public Methods (Class Functions)
+	std::string ToString();
 
+//Public static methods (public class functions that )
 private:
 	//instance variables (Class Member Variables)
+	float m_x;
+	float m_y;
 
-	//Private methods (Private class functions)
+	//Private methods (Private class functions that are static)
+
 };
 
 #endif //define(__VECTOR_2D)
